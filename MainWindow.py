ecf63c7e9d059eeca5fcdb0a9a6a422a185898ec
RPZshnik <aleksei.dediukin@gmail.com>
2021-08-19 00:17:37 +0300

    create class MainWindow

---
+++ MainWindow.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from GraphFrame import GraphFrame
+from PanelFrame import PanelFrame
+
+
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+        tk.Tk.geometry(self, "1300x600")
+        tk.Tk.wm_title(self, "Collatz")
+
+        self.graph_frame = GraphFrame(self, wigth=900, height=600)
+        self.panel_frame = PanelFrame(self)
+        self.graph_frame.pack(side=tk.LEFT, padx=10, pady=10)
+        self.panel_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
