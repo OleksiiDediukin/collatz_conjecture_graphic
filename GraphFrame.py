224d298aa7ac218df58a3406a04c352826ab3a2b
RPZshnik <aleksei.dediukin@gmail.com>
2021-08-20 19:33:05 +0300

    add drawing, update-graph methods and change init method

--- GraphFrame.py
+++ GraphFrame.py
@@ -1,24 +1,40 @@
-import tkinter as tk
+import time
 import matplotlib
+import calculations
+import tkinter as tk
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 matplotlib.use("TkAgg")
 
 
 class GraphFrame(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master)
+        self.figure = plt.figure(figsize=(10, 10), dpi=100)
+        plt.ion()
+        self.figure.add_subplot(1, 1, 1)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        fig = Figure(figsize=(10, 10), dpi=100)
-        a = fig.add_subplot(111)
-        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+    def drawing(self, start, finish):
+        for num in range(start, finish):
+            sequence = calculations.generate_sequence(num)
+            x = [0]
+            y = [num]
+            while True:
+                try:
+                    self.update_graph(x, y, sequence)
+                    time.sleep(0.2)
+                except StopIteration:
+                    break
 
-        canvas = FigureCanvasTkAgg(fig, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2Tk(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    @staticmethod
+    def update_graph(self, x, y, sequence):
+        x.append(len(x))
+        y.append(next(sequence))
+        plt.clf()
+        plt.plot(x, y)
+        plt.draw()
+        plt.gcf().canvas.flush_events()
 
