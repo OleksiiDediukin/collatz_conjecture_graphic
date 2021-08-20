f31cdaed162f2627ea67c702e3c1cb5bb0bcb6f2
RPZshnik <aleksei.dediukin@gmail.com>
2021-08-20 18:59:17 +0300

    added button click handler and input fields read method

--- PanelFrame.py
+++ PanelFrame.py
@@ -5,10 +5,16 @@ from tkinter import ttk
 class PanelFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-
-        self.draw_button = ttk.Button(self, text="Start")
+        self.master = master
+        self.draw_button = ttk.Button(self, text="Start",
+                                      command=lambda: master.start_draw(*self.get_range()))
         self.start_entry = tk.Entry(self, width=20, bd=3)
         self.finish_entry = tk.Entry(self, width=20, bd=3)
         self.start_entry.grid(row=1, column=1)
         self.finish_entry.grid(row=1, column=2)
-        self.draw_button.grid(row=2, column=1, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.draw_button.grid(row=2, column=1)
+
+    def get_range(self):
+        start = int(self.start_entry.get())
+        finish = int(self.finish_entry.get())
+        return start, finish
