a7e45d0374603bd67033976cb1e90e74e1e8f25c
RPZshnik <aleksei.dediukin@gmail.com>
2021-08-21 21:27:59 +0300

    addmaintaining the size of the axes

--- GraphFrame.py
+++ GraphFrame.py
@@ -13,9 +13,15 @@ class GraphFrame(tk.Frame):
         super().__init__(master)
         self.figure = plt.figure(figsize=(10, 10), dpi=100)
         plt.ion()
+
+        plt.gca().set_ylim([0, 1000])
+        plt.gca().set_xlim([0, 100])
         self.figure.add_subplot(1, 1, 1)
         self.canvas = FigureCanvasTkAgg(self.figure, master=self)
         self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        self.max_x = 0
+        self.max_y = 0
 
     def drawing(self, start, finish):
         for num in range(start, finish):
@@ -25,16 +31,24 @@ class GraphFrame(tk.Frame):
             while True:
                 try:
                     self.update_graph(x, y, sequence)
-                    time.sleep(0.2)
+                    time.sleep(0.002)
                 except StopIteration:
                     break
 
-    @staticmethod
-    def update_graph(x, y, sequence):
+    def update_graph(self, x, y, sequence):
         x.append(len(x))
         y.append(next(sequence))
         plt.clf()
         plt.plot(x, y)
+        self.update_max_cords(x[-1], y[-1])
+        plt.gca().set_xlim([0, max(100, self.max_x+20)])
+        plt.gca().set_ylim([0, max(1000, self.max_y+100)])
         plt.draw()
         plt.gcf().canvas.flush_events()
+
+    def update_max_cords(self, x, y):
+        if x > self.max_x:
+            self.max_x = x
+        if y > self.max_y:
+            self.max_y = y
 
