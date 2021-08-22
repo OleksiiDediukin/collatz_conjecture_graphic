99d1027572b719dd515d4d6955e8301a79476d57
RPZshnik <aleksei.dediukin@gmail.com>
2021-08-22 16:00:01 +0300

    some changes

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import ttk
 from GraphFrame import GraphFrame
 from PanelFrame import PanelFrame
 
@@ -6,12 +7,11 @@ from PanelFrame import PanelFrame
 class MainWindow(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__()
-
-        tk.Tk.geometry(self, "1300x600")
         tk.Tk.wm_title(self, "Collatz")
-
-        self.graph_frame = GraphFrame(self, wigth=900, height=600)
-        self.panel_frame = PanelFrame(self)
+        self.width = 1200
+        self.height = 600
+        self.graph_frame = GraphFrame(self, width=self.width*0.7, height=self.height)
+        self.panel_frame = PanelFrame(self, width=self.width*0.3, height=self.height)
         self.graph_frame.pack(side=tk.LEFT, padx=10, pady=10)
         self.panel_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
