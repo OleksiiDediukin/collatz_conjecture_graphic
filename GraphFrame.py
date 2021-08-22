cf00d8c834f4ad6596730a05d92e436cdd11135e
RPZshnik <aleksei.dediukin@gmail.com>
2021-08-22 15:54:35 +0300

    moved the rendering of windows into a separate method

--- GraphFrame.py
+++ GraphFrame.py
@@ -9,19 +9,26 @@ matplotlib.use("TkAgg")
 
 
 class GraphFrame(tk.Frame):
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, width=900, height=600, *args, **kwargs):
         super().__init__(master)
-        self.figure = plt.figure(figsize=(10, 10), dpi=100)
-        plt.ion()
-
-        plt.gca().set_ylim([0, 1000])
-        plt.gca().set_xlim([0, 100])
-        self.figure.add_subplot(1, 1, 1)
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
-        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
+        self.figure = None
+        self.canvas = None
+        self.width = width
+        self.height = height
         self.max_x = 0
         self.max_y = 0
+        self.rendering()
+
+    def rendering(self):
+        # self.height and self.width are in pixels, but figsize is in inches. To do this, divide by 96
+        self.figure = plt.figure(figsize=(self.width/96, self.height/96), dpi=100)
+        plt.ion()
+        self.figure.add_subplot(1, 1, 1)
+        plt.gca().set_ylim([0, 1000])
+        plt.gca().set_xlim([0, 100])
+
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def drawing(self, start, finish):
         for num in range(start, finish):
--- PanelFrame.py
+++ PanelFrame.py
@@ -3,16 +3,29 @@ from tkinter import ttk
 
 
 class PanelFrame(tk.Frame):
-    def __init__(self, master):
+    def __init__(self, master, width=300, height=600, *args, **kwargs):
         super().__init__(master)
         self.master = master
+        self.canvas = None
+        self.width = width
+        self.height = height
+        self.draw_button = None
+        self.start_entry = None
+        self.finish_entry = None
+        self.rendering()
+
+    def rendering(self):
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height)
+        self.canvas.grid()
         self.draw_button = ttk.Button(self, text="Start",
-                                      command=lambda: master.start_draw(*self.get_range()))
+                                      command=lambda: self.master.start_draw(*self.get_range()))
         self.start_entry = tk.Entry(self, width=20, bd=3)
         self.finish_entry = tk.Entry(self, width=20, bd=3)
-        self.start_entry.grid(row=1, column=1)
-        self.finish_entry.grid(row=1, column=2)
-        self.draw_button.grid(row=2, column=1)
+        tk.Label(self, text="From: ", font=("Arial", 10)).place(x=10, y=20)
+        self.start_entry.place(x=55, y=20)
+        tk.Label(self, text="To: ", font=("Arial", 10)).place(x=10, y=60)
+        self.finish_entry.place(x=55, y=60)
+        self.draw_button.place(x=12, y=100)
 
     def get_range(self):
         start = int(self.start_entry.get())
